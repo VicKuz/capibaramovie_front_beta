713168dbff3d7bfa00f36ee476c185bf4a86e942
Victor Kuznietsov <capybaraandgroot@gmail.com>
2023-02-24 17:56:01 +0200

    header rebuild 1

--- master/resources/js/sticky-header.js
+++ master/resources/js/sticky-header.js
@@ -1,6 +1,5 @@
 /* !!!!!!!!!!!!!!!!!!!!! ПЕРЕВІРЯТИ ЧИ Є HERO, якщо ні - то одразу sticky !!!!!!!!!!!!!!!!! */
 
-
 let root = document.documentElement;
 
 function changeValue(id, value) {
@@ -9,12 +8,12 @@ function changeValue(id, value) {
 
 
 function vh(percent) {
-    var h = Math.max(document.documentElement.clientHeight, window.innerHeight || 0);
+    let h = Math.max(document.documentElement.clientHeight, window.innerHeight || 0);
     return (percent * h) / 100;
 }
 
 function vw(percent) {
-    var w = Math.max(document.documentElement.clientWidth, window.innerWidth || 0);
+    let w = Math.max(document.documentElement.clientWidth, window.innerWidth || 0);
     return (percent * w) / 100;
 }
 
@@ -26,45 +25,36 @@ function vmax(percent) {
     return Math.max(vh(percent), vw(percent));
 }
 
+const Hero = document.querySelector(".Hero");
+const herocontent = document.querySelector(".hero-content");
+const header = document.querySelector("header");
 
 document.addEventListener('DOMContentLoaded', () => {
-    if(window.innerWidth > 640){
-        window.onscroll = function() {headerScroll()};
+
+    if (Hero) {
+        header.classList.add("hero-header");
+    } else {
+        header.classList.remove("hero-header");
     }
 })
 
-window.addEventListener('resize', function(){
-    if(window.innerWidth > 640){
-        window.onscroll = function() {headerScroll()};
-    }
-});
 
-function headerScroll() {
-
-    let header = document.getElementById("header");
-
-    if (document.body.scrollTop > vw(2.5) || document.documentElement.scrollTop > vw(2.5)) {
-        header.classList.add("sticky");
-    } else {
-        header.classList.remove("sticky");
-    }
-
-    /*!!!!!!!!!!!!!!!!!Зробити ініціалізацію тільки на інтервалі прокрутки коли треба!!!!!!!!!!!!*/
-    var header_opacity = (1 - window.pageYOffset/vw(1.5)) * 0.3 + 0.7;
-
-    if (header_opacity < '0.7') {
-        header.style.opacity = '0.7';
-    } else if (header_opacity >= '0.7') {
-        header.style.opacity = header_opacity;
-    }
-
-    /* Переробити vw(4) в івент sticked і записування значення скролу для розрахунку) */
-    var bg_opacity = ((1 - (vh(100) - (window.pageYOffset - vw(4))) / vh(100)));
-        changeValue('header-background', 'rgba(0, 0, 0, ' + bg_opacity + ')');
-    if (bg_opacity > '1') {
-        changeValue('header-background', 'rgba(0, 0, 0, 1)');
-    } else if (bg_opacity < '0') {
-        changeValue('header-background', 'rgba(0, 0, 0, 0)');
-    }
-
+const headerObserverOptions = {
+    rootMargin: '0px',
+    threshold: 1
 }
+
+
+const headerexposed = (entries) => {
+    entries.forEach(entry => {
+        if (entry.isIntersecting) {
+            header.classList.remove("exposed")
+        } else {
+            header.classList.add("exposed")
+        }
+    });
+}
+
+const observer = new IntersectionObserver(headerexposed, headerObserverOptions)
+
+observer.observe(herocontent);
